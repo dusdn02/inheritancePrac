9f54bebdd70e7fdf9d7c8c94bc1958b522388ab5
dusdn02 <lyw9114@naver.com>
2020-06-10 14:01:13 +0900

    초기 커밋.

---
+++ inheritancePrac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritancePrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+            
+            foreach(var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+        }
+    }
+}
