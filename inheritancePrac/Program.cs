738d179c039daddeb3261d881c3cfafae3893b51
dusdn02 <lyw9114@naver.com>
2020-06-12 12:48:47 +0900

    base()

--- inheritancePrac/Program.cs
+++ inheritancePrac/Program.cs
@@ -11,22 +11,24 @@ namespace inheritancePrac
         static void Main(string[] args)
         {
 
-            List<Animal> animals =
-                new List<Animal>
-                {
-                    new Dog(), new Cat(), new Cat(), new Dog(), new Dog(), new Cat() };
-            
-            foreach(var item in animals)
-            {
-                item.Eat();
-                item.Sleep();
+            //List<Animal> animals =
+            //    new List<Animal>
+            //    {
+            //        new Dog(), new Cat(), new Cat(), new Dog(), new Dog(), new Cat() };
 
-                var dog = item as Dog;
-                if (dog != null) { dog.Bark(); }
+            //foreach(var item in animals)
+            //{
+            //    item.Eat();
+            //    item.Sleep();
 
-                var cat = item as Cat;
-                if (cat != null) { cat.Meow(); }
-            }
+            //    var dog = item as Dog;
+            //    if (dog != null) { dog.Bark(); }
+
+            //    var cat = item as Cat;
+            //    if (cat != null) { cat.Meow(); }
+            //}
+
+            Child c = new Child(11);
 
         }
     }
